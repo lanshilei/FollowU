1471d0e9d7234e69f38eac2fe1dffd780cec07db
lanshilei <531589402@qq.com>
2020-07-13 21:55:54 +0800

    update host

--- src/utils/global_data.js
+++ src/utils/global_data.js
@@ -2,7 +2,8 @@
  * 这里放一些全局变量
  */
 const globalData = {}
-export const BASE_URL = "http://49.233.15.125:33000/followU/"
+// export const BASE_URL = "http://49.233.15.125:33000/followU/"
+export const BASE_URL = "https://www.followu.top/followU/"
 export const KEY_TOKEN = 'global_key_token'
 export const KEY_USERID = "global_keu_userid"
 export const TEMPLATE_IDS = ['0zxDmA9u0TTJqy8EpIFlJoW4bC9q-nt3CA6liiXqxA0']
